2d74b94f5ff158a90f395ec07294d8b3405d98ab
dks0808 <73996305+dks0808@users.noreply.github.com>
2024-01-24 18:01:43 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+    cin >> a;
+    cin >>b;
+    float c = a + b;
+    cout.precision(3);
+    cout << showpoint << c<<endl;
+
+
+    return 0;
+}
