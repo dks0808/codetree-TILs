9bcb84b3ad2655ba37d404b6dfdf7dcffc6ad04a
dks0808 <73996305+dks0808@users.noreply.github.com>
2024-01-24 18:10:11 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    float a;
+    float b;
+    float c;
+    
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout << fixed << setprecision(3) << a<<endl <<b<<endl<< c<<endl;
+    return 0;
+}
