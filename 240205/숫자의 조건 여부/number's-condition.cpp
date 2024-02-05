8805f53b95ef554f18ca50258bebb18483136f22
dks0808 <73996305+dks0808@users.noreply.github.com>
2024-02-05 14:37:06 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240205/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >>a ;
+    if(-1000<=a<=1000)
+    {
+        if(a>=133)
+        {
+            cout <<1;
+        }
+        else
+        {
+            cout <<0;
+        }
+    }
+    return 0;
+}
