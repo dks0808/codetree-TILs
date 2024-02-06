0a4733576015219e5703c2132d779b4599f3800f
dks0808 <73996305+dks0808@users.noreply.github.com>
2024-02-06 18:14:32 +0900

    Title: 물의 상태, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240206/물의 상태/state-of-water.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int temperature;
+    cin >> temperature;
+    if(temperature<=0)
+    {
+        cout << "ice";
+
+    }
+    else
+    {
+        cout <<"water";
+    }
+    return 0;
+}
